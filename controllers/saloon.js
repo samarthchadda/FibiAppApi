6721d3a0516b756b06369b766e27a7b23ebac0e6
samarthchadda <samarthchadda.it20@jecrc.ac.in>
2021-02-26 22:35:23 +0530

    nearSaloons7

--- controllers/saloon.js
+++ controllers/saloon.js
@@ -446,6 +446,16 @@ exports.getSingleSaloon=(req,res,next)=>{
 
 exports.getDiffSaloon=(req,res,next)=>{
 
+    let curr = new Date();
+let week = []
+
+for (let i = 1; i <= 7; i++) {
+  let first = curr.getDate() - curr.getDay() + i 
+  let day = new Date(curr.setDate(first)).toISOString().slice(0, 10)
+  week.push(day)
+}
+console.log("Week : ", week);
+
     const latitude = +req.body.latitude;
     const longitude = +req.body.longitude;
     const range = +req.body.range;
