3be9d4af1a74f52e1a8d0f972a211c0479df99d3
samarthchadda <samarthchadda.it20@jecrc.ac.in>
2021-03-06 11:04:17 +0530

    nearSaloonFinal

--- controllers/saloon.js
+++ controllers/saloon.js
@@ -454,7 +454,8 @@ let week = []
 for (let i = 1; i <= 7; i++) {
 let first = curr.getUTCDate() -( curr.getUTCDay() || 7) + i 
 console.log("First:",first);
-let day = new Date(curr.setDate(first)).toISOString().slice(0, 10)
+let day = new Date(curr.setDate(first)).toISOString().slice(0, 10);
+console.day(day)
 week.push(day)
 }
 console.log("Week : ",week);
