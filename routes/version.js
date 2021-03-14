2895869e0140e6469c1a9f4deee6bdc8bea97bde
samarthchadda <samarthchadda.it20@jecrc.ac.in>
2021-03-14 14:30:41 +0530

    version2

--- routes/version.js
+++ routes/version.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const Version = require('../models/version');
 
 
-router.post('/get-app-version',(req,res,next)=>{
+router.get('/get-app-version',(req,res,next)=>{
 
     Version.fetchAppVersion()
     .then(versionData=>{
 
         res.json({status:true,version:versionData});
-        
+
     })
     .catch(err=>{
         res.json({status:false,err:err});
