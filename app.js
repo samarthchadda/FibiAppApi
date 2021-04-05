8a82c3a018e2848347de569fd1d17b425868198e
samarthchadda <samarthchadda.it20@jecrc.ac.in>
2021-04-05 18:18:59 +0530

    https15

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const mongoConnect = require('./util/database').mongoConnect;
 
 const fs = require('fs');
 var https = require('https');
-var privateKey  = fs.readFileSync('./sslcert/c3b1fabb5b44ce7a.pem',{ encoding: "utf8" });
-var certificate = fs.readFileSync('./sslcert/c3b1fabb5b44ce7a.crt','utf-8');
+// var privateKey  = fs.readFileSync('./sslcert/c3b1fabb5b44ce7a.pem','utf-8');
+// var certificate = fs.readFileSync('./sslcert/c3b1fabb5b44ce7a.crt','utf-8');
 
-var credentials = {key: privateKey, cert: certificate};
+// var credentials = {key: privateKey, cert: certificate};
 
 // var options = {
 //   key: fs.readFileSync('./key.pem', 'utf8'),
@@ -97,16 +97,16 @@ app.use('/api',versionRoutes);
 app.use('/api',paymentRoutes);
 
 
-var httpsServer = https.createServer(credentials, app);
+// var httpsServer = https.createServer(credentials, app);
 
-let port = process.env.PORT || 8443;
+let port = process.env.PORT || 8000;
 //establishing DB connection
 mongoConnect(()=>{
      
     //listening to incoming request on this port
    
     app.listen(port);
-    httpsServer.listen(port);
+    // httpsServer.listen(port);
 
 });
 // httpsServer.listen(PORT, logger.info(`Server listening on port: ${PORT}`));
